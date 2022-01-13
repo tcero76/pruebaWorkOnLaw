c6bc77ca8ac80818b8fd3336c9f0c9669c6992da
leonardo <leonardo.lastra@gmail.com>
2022-01-13 19:01:07 -0300

    modifica test, actualiza fin periodo

--- test/controllers/postulaciones_controller_test.rb
+++ test/controllers/postulaciones_controller_test.rb
@@ -3,22 +3,24 @@ require "test_helper"
 class PostulacionesControllerTest < ActionDispatch::IntegrationTest
     test "Verificar experiencia de nikola@tesla.org" do
         get "/postulaciones?email=nikola@tesla.org"
-        years = JSON.parse(@response.body)
+        res = JSON.parse(@response.body)
         diff = (Date.today - Date.parse("2020-03-15")).to_i
         antiquity = (( 584.0 + diff) / 365).to_d.round(1)
-        assert_equal antiquity, years["work_experience_years"].to_d
+        assert_equal antiquity, res["work_experience_years"].to_d
     end
 
     test "Verificar experiencia de jane@goodall.org" do
         get "/postulaciones?email=jane@goodall.org"
-        years = JSON.parse(@response.body)
-        assert_equal 14.7, years["work_experience_years"].to_d
+        res = JSON.parse(@response.body)
+        diff = (Date.today - Date.parse("2007-04-27")).to_i
+        antiquity = (diff.to_d / 365.0).to_d.round(1)
+        assert_equal 14.7, res["work_experience_years"].to_d
     end
 
     test "Verificar experiencia de isaac@asimov.org" do
         get "/postulaciones?email=isaac@asimov.org"
-        years = JSON.parse(@response.body)
-        assert_equal 0.0, years["work_experience_years"].to_d
+        res = JSON.parse(@response.body)
+        assert_equal 0.0, res["work_experience_years"].to_d
     end
       
 end
