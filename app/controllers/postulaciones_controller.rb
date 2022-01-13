3b0da99aecc1b4ba082f90e356a19599127ff0f3
leonardo <leonardo.lastra@gmail.com>
2022-01-13 20:53:13 -0300

    Control de errores

--- app/controllers/postulaciones_controller.rb
+++ app/controllers/postulaciones_controller.rb
@@ -7,7 +7,11 @@ class PostulacionesController < ApplicationController
         uri = URI('https://api.workon.law/technical_challenge/get_lawyers')
         res = Net::HTTP.get(uri)
         can = JSON.parse(res)
-        works = can["candidates"].select{|c| c["email"] == email}.first["works"]
+        works = can["candidates"].select{|c| c["email"] == email}
+        if works.length==0
+            return render json: {"email" => nil , "work_experience_years" => nil}
+        end
+        works = works.first["works"]
         arr = (Date.parse("2000-01-01")..Date.today).to_a
         count = 0.0
         arr.each do |d|
